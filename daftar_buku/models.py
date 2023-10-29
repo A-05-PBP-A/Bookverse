e53dbe1c991e7b22e6ad548ef2e025791fe5656a
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2023-10-29 13:10:49 +0700

    display dan book profile linked

--- daftar_buku/models.py
+++ daftar_buku/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Main(models.Model):
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
--- daftar_buku/views.py
+++ daftar_buku/views.py
@@ -5,20 +5,20 @@ from django.core import serializers
 from daftar_buku.models import Main
 from bookProfile.models import Book
 
+from django.utils import timezone
+
 book = Book.objects.all()
 
 def show_main(request):
-    date = Main.objects.all()
-    #book = Book.objects.all()
+    tanggal = Main.objects.all()
     context = {
         'buku' : book,
-        'tanggal' : date,
+        'tanggal' : tanggal,
     }
 
     return render(request, "landing_page.html", context)
 
 def get_books_json(request):
-    #book = Book.objects.all()
     return HttpResponse(serializers.serialize('json', book))
 
 def filter_books(request):
@@ -26,22 +26,14 @@ def filter_books(request):
     filter_type = request.GET.get('filter_type', '')
 
     if filter_type == 'title':
-        #filtered_books = Book.objects.filter(title__icontains=keyword)
         filtered_books = book.filter(title__icontains=keyword)
     elif filter_type == 'author':
-        #filtered_books = Book.objects.filter(author__icontains=keyword)
         filtered_books = book.filter(author__icontains=keyword)
     elif filter_type == 'publication_year':
-        #filtered_books = Book.objects.filter(publication_year__icontains=keyword)
         filtered_books = book.filter(publication_year__icontains=keyword)
     elif filter_type == 'publisher':
-        #filtered_books = Book.objects.filter(publisher__icontains=keyword)
         filtered_books = book.filter(publisher__icontains=keyword)
     else:
-        #filtered_books = Book.objects.all()
         filtered_books = book
 
     return HttpResponse(serializers.serialize('json', filtered_books))
-    #return JsonResponse(filtered_books, safe=False)
-    #data = [{'image_url_l': book.image_url_l, 'title': book.title, 'author': book.author, 'publication_year': book.publication_year, 'publisher': book.publisher} for book in filtered_books]
-    #return JsonResponse(data, safe=False)
