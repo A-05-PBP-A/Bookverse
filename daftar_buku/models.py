778cac50fcdd941b63eadd5399abf9c922b38deb
astrialyanda <alyandaastri19@gmail.com>
2023-10-29 18:21:14 +0700

    add feedback form

--- daftar_buku/models.py
+++ daftar_buku/models.py
@@ -4,3 +4,15 @@ from datetime import date
 # Create your models here.
 class Main(models.Model):
     current_date = models.DateField(default=date.today)
+
+class Feedback(models.Model):
+    RATE_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+    name = models.CharField(max_length=255)
+    feeds = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICES)
--- daftar_buku/views.py
+++ daftar_buku/views.py
@@ -1,17 +1,21 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.core import serializers
+from django.urls import reverse
+from daftar_buku.forms import FeedbackForm
 
-from daftar_buku.models import Main
+from daftar_buku.models import Main, Feedback
 from bookProfile.models import Book
 
 book = Book.objects.all()
 
 def show_main(request):
     date = Main.objects.all()
+    feedback = Feedback.objects.all()
     context = {
         'buku' : book,
         'tanggal' : date,
+        'feedback' : feedback,
     }
 
     return render(request, "landing_page.html", context)
@@ -24,25 +28,28 @@ def filter_books(request):
     filter_type = request.GET.get('filter_type', '')
 
     if filter_type == 'title':
-        #filtered_books = Book.objects.filter(title__icontains=keyword)
         filtered_books = book.filter(title__icontains=keyword)
     elif filter_type == 'author':
-        #filtered_books = Book.objects.filter(author__icontains=keyword)
         filtered_books = book.filter(author__icontains=keyword)
     elif filter_type == 'publication_year':
-        #filtered_books = Book.objects.filter(publication_year__icontains=keyword)
         filtered_books = book.filter(publication_year__icontains=keyword)
     elif filter_type == 'publisher':
-        #filtered_books = Book.objects.filter(publisher__icontains=keyword)
         filtered_books = book.filter(publisher__icontains=keyword)
     else:
-        #filtered_books = Book.objects.all()
         filtered_books = book
 
     return HttpResponse(serializers.serialize('json', filtered_books))
-    #return JsonResponse(filtered_books, safe=False)
-    #data = [{'image_url_l': book.image_url_l, 'title': book.title, 'author': book.author, 'publication_year': book.publication_year, 'publisher': book.publisher} for book in filtered_books]
-    #return JsonResponse(data, safe=False)
 
-# def go_to_profile(request, item_pk):
-#     item = book.get(item_pk)
+def feedback_form(request):
+    form = FeedbackForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('daftar_buku:show_main'))
+
+    context = {'form': form}
+    return render(request, "feedback_form.html", context)
+
+def show_feedback_json(request):
+    feedback = Feedback.objects.all()
+    return HttpResponse(serializers.serialize("json", feedback), content_type="application/json")
