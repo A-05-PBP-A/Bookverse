1adf5fe2cb8084ff008a751adcc24608d09fca20
RyanAfzal <ryanafzal3@gmail.com>
2023-12-17 18:45:40 +0700

    new function to support flutter

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from userProfile.views import show_user_profile, edit_profile, add_to_favorites, get_user_favorite, change_password
+from userProfile.views import add_to_favorites_flutter, get_user_favorite_flutter, delete_book
 
 app_name = 'userProfile'
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('add_to_favorites/<int:book_id>/', add_to_favorites, name='add_to_favorites'),
     path('change_password', change_password,name='change_password'),
     path('book_favorite/',get_user_favorite, name='get_user_favorite'),
+    path('favorite-flutter/',add_to_favorites_flutter, name='add_to_favorites_flutter'),
+    path('book_favorite_flutter/<str:username>/', get_user_favorite_flutter, name='get_user_favorite_flutter'),
+    path('delete_bookFav/<int:book_id>/', delete_book, name='delete_book'),
 ]
