10442a7573795d4353514acc02ec15d09d50bc2f
lucindalaurent <lucindalaurent21@gmail.com>
2023-12-17 21:01:16 +0700

    support borrowreturn flutter

--- borrowreturn/urls.py
+++ borrowreturn/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from borrowreturn.views import borrow_book, get_book_by_id, show_borrowing, get_user_borrowing, return_borrowing, filter_borrowings
+from borrowreturn.views import borrow_book, get_book_by_id, show_borrowing, get_user_borrowing, return_borrowing, filter_borrowings, return_borrowing_flutter, borrow_book_flutter, get_book_url
 
 app_name = 'borrowreturn'
 
@@ -9,5 +9,8 @@ urlpatterns = [
     path('borrowing-data/',get_user_borrowing, name='get_user_borrowing' ),
     path('book-data/<int:book_id>/', get_book_by_id, name='get_book_by_id'),
     path('return-borrowing/<int:borrowing_id>/', return_borrowing, name='return_borrowing'),
-    path('filter-borrowings/', filter_borrowings, name='filter_borrowings')
+    path('filter-borrowings/', filter_borrowings, name='filter_borrowings'),
+    path('return-flutter/', return_borrowing_flutter, name='return_borrowing_flutter'),
+    path('borrow-flutter/', borrow_book_flutter, name='borrow_book_flutter'),
+    path('get-book-cover/', get_book_url, name='get_book_url')
 ]
--- borrowreturn/views.py
+++ borrowreturn/views.py
@@ -11,6 +11,7 @@ from django.core import serializers
 from django.utils import timezone
 from userProfile.models import UserHistory
 from userProfile.forms import bookHistoryForm
+import json
 
 @login_required(login_url='/login/')
 def borrow_book(request):
@@ -56,9 +57,10 @@ def get_book_by_id(request, book_id):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 #untuk halaman borrowing flutter
+@csrf_exempt
 def get_user_borrowing(request):
     items = Borrowing.objects.filter(user=request.user, is_returned = False)
-    return HttpResponse(serializers.serialize('json', items))
+    return HttpResponse(serializers.serialize('json', items), content_type="application/json")
 
 @csrf_exempt
 def return_borrowing(request, borrowing_id):
@@ -90,3 +92,69 @@ def filter_borrowings(request):
    
     return HttpResponse(serializers.serialize('json', filtered_books))
 
+@csrf_exempt
+def return_borrowing_flutter(request):
+    formHistory = bookHistoryForm(None)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        borrowing_id = int(data["id"])
+        borrowing = Borrowing.objects.get(id = borrowing_id)
+        book = borrowing.book
+        borrowing.is_returned = True
+
+        # existing_History = UserHistory.objects.filter(user=request.user, book=book)
+
+        # #untuk menambah ke history setelah di return
+        # if not existing_History.exists():
+        #     bookHistory = formHistory.save(commit=False)
+        #     bookHistory.user = request.user
+        #     bookHistory.book = book
+        #     bookHistory.book_title = bookHistory.book.title
+        #     bookHistory.image_url_l = bookHistory.book.image_url_l
+        #     bookHistory.reference_id = bookHistory.book.pk
+        #     bookHistory.save()
+            
+        borrowing.real_return_date = timezone.now()
+        borrowing.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+@csrf_exempt
+def borrow_book_flutter(request):
+    form = BorrowingForm(request.POST or None)
+    if request.method == 'POST':
+        now = timezone.now()
+        data = json.loads(request.body)
+        book = Book.objects.get(pk=data["book"])
+        now = timezone.now()
+        # mencari semua buku yang belum dikembalikan oleh user dan melebihi return_date
+        overdue_books = Borrowing.objects.filter(user = request.user, is_returned=False, return_date__lt=now)
+        # Cek apakah buku ini sudah dipinjam oleh user dan belum dikembalikan
+        existing_borrowing = Borrowing.objects.filter(user=request.user, book=book, is_returned=False)
+        if existing_borrowing.exists():
+            return JsonResponse({'message': 'You have already borrowed this book and it has not been returned yet.', 'status': 'error'}, status=400)
+        elif overdue_books.exists():
+            return JsonResponse({'message': 'You have overdue book(s). Please return them before borrowing another one.', 'status': 'error'}, status=400)
+        else:
+            new_borrowing = form.save(commit=False)
+            new_borrowing.user = request.user
+            new_borrowing.book = book
+            new_borrowing.book_title = new_borrowing.book.title
+            new_borrowing.image_url_l = new_borrowing.book.image_url_l
+            new_borrowing.reference_id = new_borrowing.book.pk
+            new_borrowing.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
+@csrf_exempt
+def get_book_url(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        book_id = int(data["id"])
+        book = Book.objects.get(id = book_id)
+        book_url = book.image_url_l
+
+        return JsonResponse({"url": book_url}, status = 200)
